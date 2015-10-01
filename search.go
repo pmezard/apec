6e90e67ece20ced609c1420643192a5b8218ea68
Patrick Mezard <patrick@mezard.eu>
2015-10-01 10:25:28 +0200

    search: sort offers by ascending date

--- search.go
+++ search.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"github.com/blevesearch/bleve"
 	"path/filepath"
+	"sort"
+	"strings"
 )
 
 type jsonOffer struct {
 	Id          string `json:"numeroOffre"`
 	Title       string `json:"intitule"`
-	Date        string `json:"dataPublication"`
+	Date        string `json:"datePublication"`
 	Salary      string `json:"salaireTexte"`
 	PartialTime bool   `json:"tempsPartiel"`
 	Location    string `json:"lieuTexte"`
@@ -22,19 +24,49 @@ func (offer *jsonOffer) Type() string {
 	return "offer"
 }
 
-func printOffer(store *Store, id string) error {
-	data, err := store.Get(id)
-	if err != nil {
-		return err
+type offersByDate []*jsonOffer
+
+func (s offersByDate) Len() int {
+	return len(s)
+}
+
+func (s offersByDate) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s offersByDate) Less(i, j int) bool {
+	return s[i].Date < s[j].Date
+}
+
+func formatDate(s string) string {
+	parts := strings.SplitN(s, "T", 2)
+	return parts[0]
+}
+
+func printOffers(store *Store, ids []string) error {
+	// Load offer documents
+	offers := []*jsonOffer{}
+	for _, id := range ids {
+		data, err := store.Get(id)
+		if err != nil {
+			return err
+		}
+		offer := &jsonOffer{}
+		err = json.Unmarshal(data, offer)
+		if err != nil {
+			return err
+		}
+		offers = append(offers, offer)
 	}
-	offer := &jsonOffer{}
-	err = json.Unmarshal(data, offer)
-	if err != nil {
-		return err
+	// Sort by ascending publication date
+	sorted := offersByDate(offers)
+	sort.Sort(sorted)
+	for _, offer := range sorted {
+		fmt.Printf("%s %s %s %s (%s)\n", offer.Id, offer.Title, offer.Salary,
+			offer.Account, formatDate(offer.Date))
+		fmt.Printf("    https://cadres.apec.fr/offres-emploi-cadres/offre.html?numIdOffre=%s\n",
+			offer.Id)
 	}
-	fmt.Printf("%s %s %s %s\n", offer.Id, offer.Title, offer.Salary, offer.Account)
-	fmt.Printf("    https://cadres.apec.fr/offres-emploi-cadres/offre.html?numIdOffre=%s\n",
-		offer.Id)
 	return nil
 }
 
@@ -59,19 +91,19 @@ func search() error {
 	q := bleve.NewQueryStringQuery(*searchQuery)
 	rq := bleve.NewSearchRequest(q)
 	rq.Size = 100
+	ids := []string{}
 	for {
 		res, err := index.Search(rq)
 		if err != nil {
 			return err
 		}
 		for _, doc := range res.Hits {
-			printOffer(store, doc.ID)
+			ids = append(ids, doc.ID)
 		}
 		if len(res.Hits) < rq.Size {
 			break
 		}
 		rq.From += rq.Size
 	}
-	_ = store
-	return nil
+	return printOffers(store, ids)
 }
